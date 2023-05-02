04e78decf3b2b4c9456f80d7c53e512334d3e0b9
Taizi Huang <htzllt@gmail.com>
2023-05-03 06:54:18 +0800

    Update wmyblog_coron.py

--- wmyblog_coron.py
+++ wmyblog_coron.py
@@ -243,7 +243,7 @@ def updateBlogData(nTask=20, proxy='',articleUpdate=True,commentFullUpdate=False
         if i.find("div",{'role':"link"}):
             i.find("div",{'role':"link"}).decompose()
         df = pd.concat([df, pd.DataFrame(data={'filename': i.text, 'id': i['data-id']}, index=[0])], ignore_index=True)
-    L = tasker.run([saveScript(df.loc[idx, 'filename'][:6], df.loc[idx, 'id'], proxy) for idx in df.index])
+    #L = tasker.run([saveScript(df.loc[idx, 'filename'][:6], df.loc[idx, 'id'], proxy) for idx in df.index])
     print('transcript downloaded')
 
     if articleUpdate:
