71743227d19599e316173a35a0fcc217d35a7440
Taizi Huang <htzllt@gmail.com>
2023-07-12 08:17:52 +0800

    Update wmyblog_coron.py

--- wmyblog_coron.py
+++ wmyblog_coron.py
@@ -53,7 +53,7 @@ def updateTag(tagFile='./data/tag.pkl'):
         'Apikey':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqbG9za2pjb2dya3ZocGtmb296Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYwMjIyNjIsImV4cCI6MjAwMTU5ODI2Mn0.peYwcTSZcDd3SvG5Rh99jlM7uyHkUjq1klvqRt2vF5c',
         'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqbG9za2pjb2dya3ZocGtmb296Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYwMjIyNjIsImV4cCI6MjAwMTU5ODI2Mn0.peYwcTSZcDd3SvG5Rh99jlM7uyHkUjq1klvqRt2vF5c'
     }
-    tag_dict = json.loads(requests.get('https://cjloskjcogrkvhpkfooz.supabase.co/rest/v1/tag?select=*&order=created_at.desc&limit=200',headers=headers).content)
+    tag_dict = json.loads(requests.get('https://cjloskjcogrkvhpkfooz.supabase.co/rest/v1/tag?select=*&order=created_at.desc&limit=500',headers=headers).content)
     df_tag_new = pd.DataFrame(tag_dict).rename(columns={'id':'md5'})[['md5','tag']]
     df_tag = pd.concat([df_tag_new,df_tag],ignore_index=True).drop_duplicates(keep='first').reset_index(drop=True)
     df_tag.to_pickle(tagFile)
