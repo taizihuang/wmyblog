bd47c624b805128837a4ea9b7424748da61e2634
Taizi Huang <htzllt@gmail.com>
2024-05-11 14:12:16 +0800

    1

--- wmyblog_coron.py
+++ wmyblog_coron.py
@@ -822,5 +822,5 @@ def updateBlogPage(days=7,articleFile="./data/article_full.pkl",commentFile="./d
     print('search data generated')
 
 if __name__ == "__main__":
-    updateBlogData(nTask=2, proxy='' ,gDriveUpdate=False)
+    # updateBlogData(nTask=2, proxy='' ,gDriveUpdate=False)
     updateBlogPage(days=25)
