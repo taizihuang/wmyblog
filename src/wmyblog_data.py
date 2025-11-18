816c2d352b9cf82eca5df33ea711acefab81e54e
Taizi Huang <htzllt@gmail.com>
2025-11-18 12:09:41 +0800

    1

--- src/wmyblog_data.py
+++ src/wmyblog_data.py
@@ -194,19 +194,26 @@ def update_data(data_dir, img_dir):
     df_article = pd.read_pickle(article_file)
     df_comment = pd.read_pickle(comment_file)
 
+
     headers = {
-        'referer' : 'https://blog.udn.com/MengyuanWang',
-        'host' : 'blog.udn.com',
-        'Origin' : 'https://blog.udn.com',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'accept-encoding': 'gzip, deflate, br, zstd',
-        'accept-language': 'en-US,en;q=0.9,zh-TW;q=0.8,zh-CN;q=0.7,zh;q=0.6',
-        'Dnt': '1',
-        'priority': 'u=0, i',
-        'sec-ch-ua': '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
-        'sec-ch-ua-mobile': '?0',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
-        'Content-Type': 'application/html'
+               "pragma": "no-cache",
+               "cache-control": "no-cache",
+               "sec-ch-ua": "\"Chromium\";v=\"142\", \"Microsoft Edge\";v=\"142\", \"Not_A Brand\";v=\"99\"",
+               "sec-ch-ua-mobile": "?0",
+               "sec-ch-ua-platform": "\"Windows\"",
+               "dnt": "1",
+               "upgrade-insecure-requests": "1",
+               "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0",
+               "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+               "sec-fetch-site": "same-origin",
+               "sec-fetch-mode": "navigate",
+               "sec-fetch-user": "?1",
+               "sec-fetch-dest": "document",
+               "referer": "https://blog.udn.com/MengyuanWang/article",
+               "accept-encoding": "gzip, deflate, br, zstd",
+               "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh-TW;q=0.7,zh;q=0.6,en-CA;q=0.5",
+               "cookie": "_td=9505f9ed-d13b-4495-b48e-0b09e6caa60a",
+               "priority": "u=0, i"
     }
 
     id_list = []
