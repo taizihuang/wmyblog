ed777cd5df17ec4a5ee091365b1fa6ee8f026874
Taizi Huang <htzllt@gmail.com>
2026-09-18 01:25:08 +0000

    1

--- src/article_merger.py
+++ src/article_merger.py
@@ -289,6 +289,8 @@ class ArticleMerger:
                 else:
                     break
         content = content.replace("戦", "戰")
+        content = content.replace("\r\n", "")
+        content = content.replace("\r", "")
         content = re.sub(r"<span style=.*?>(.*?)<\/span>", r"\1", content)
         return content
     
--- src/wmyblog.py
+++ src/wmyblog.py
@@ -483,6 +483,7 @@ class Wmyblog:
             if deleted:
                 comment = f"<strike>{comment}</strike>"
             comment = comment.replace('\r\n','<br><br>')
+            comment = comment.replace('\r','')
             comment = comment.replace('\n','<br><br>')
             comment = comment.replace('<br><br><br>','<br>')
             comment = comment.replace('<br><br><br>','<br>')
@@ -544,6 +545,8 @@ class Wmyblog:
         html = Template(filename=article_template_file).render(post_data=post_data,
                                                                note_data=note_data,
                                                                comment_data=comment_data)
+        html = html.replace("\r\n", "")
+        html = html.replace("\r", "")
         with open(f"{self.html_dir}/{art_id}.html", "w", encoding="utf8") as f:
             f.write(html)
     
